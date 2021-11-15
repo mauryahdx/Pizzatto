1eb0ce32542c4eaa7df2dc912f763915ff7e306d
mauryahdx <suryamourya@iiitdmj.ac.in>
2021-11-15 14:44:46 +0530

    make home page

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
+//Assets 
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
     res.render('home')
 })
+
+
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
